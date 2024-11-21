8182a45e20474325fee2e099affe03a20fb5bd1a
chenjiahao <834713285@qq.com>
2024-11-21 22:53:50 +0800

    init

---
+++ src/main/java/pers/jhshop/common/consts/ApiConstants.java
@@ -0,0 +1,24 @@
+package pers.jhshop.common.consts;
+
+/**
+ * 接口常量前缀
+ * @author ChenJiahao(五条)
+ * @date 2024/11/21 22:53:13
+ */
+public interface ApiConstants {
+
+    /**
+     * 后台系统功能接口前缀
+     */
+    String API_BIZ = "biz/";
+
+    /**
+     * 用户端接口默认前缀
+     */
+    String API_USER = "hm-api/";
+
+    /**
+     * feign调用默认接口前缀
+     */
+    String API_FEIGN = "fapi/";
+}
